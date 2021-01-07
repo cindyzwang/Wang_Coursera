f61365973de3bbd7754bb18a107416fd3c247b3e
Cindy Wang <cindy.wang@synthego.com>
2021-01-06 18:36:50 -0800

    remove unnecessary mains

--- course1/src/stats.c
+++ course1/src/stats.c
@@ -30,29 +30,6 @@
 /* Size of the Data Set */
 #define SIZE (40)
 
-int main() {
-
-  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
-                              114, 88,   45,  76, 123,  87,  25,  23,
-                              200, 122, 150, 90,   92,  87, 177, 244,
-                              201,   6,  12,  60,   8,   2,   5,  67,
-                                7,  87, 250, 230,  99,   3, 100,  90};
-
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
-  PRINTF("Running analysis on the set:\n");
-  print_array(test, SIZE);
-  PRINTF("------------- RESULTS -------------\n");
-  
-  print_statistics(test, SIZE);
-  
-  PRINTF("------------- POST ANALYSIS -------------\n");
-  PRINTF("The sorted data set is:\n");
-  print_array(test, SIZE);
-  
-  return 0;
-}
-
 /* Add other Implementation File Code Here */
 void print_statistics(unsigned char data[], unsigned int length) {
   sort_array(data, length);
